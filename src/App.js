f6eb7331e8eca19b76f758ceb959d9059041fadc
Vikas Chhonkar <vikaschhonkar1@gmail.com>
2023-09-09 18:16:22 +0530

    Set up React-Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import EditorPage from "./pages/EditorPage";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/editor/:roomId" element={<EditorPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
